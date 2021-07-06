134c9b6f39b15c66ac44765862236c2fa151996c
Ron San Jose <ronsj1@gmail.com>
2021-07-05 23:20:04 -0700

    check for page data

--- src/pages/[[...slug]].js
+++ src/pages/[[...slug]].js
@@ -14,9 +14,9 @@ export default function Slug({ preview, page }) {
   return (
     <div style={{ padding: '2rem' }}>
       <Head>
-        <title>{page.title}</title>
+        <title>{page?.title}</title>
       </Head>
-      <Content content={page.content}></Content>
+      <Content content={page?.content}></Content>
       <Json data={page} />
     </div>
   )
@@ -34,17 +34,35 @@ function Json({ data, tabSize = 2 }) {
   return <pre>{JSON.stringify(data, null, tabSize)}</pre>
 }
 
+/**
+ *
+ *  INCREMENTAL STATIC REGENERATION
+ *  https://nextjs.org/docs/basic-features/data-fetching#incremental-static-regeneration
+ *
+ */
+
+// This function gets called at build time on server-side.
+// It may be called again, on a serverless function, if
+// the path has not been generated.
 export async function getStaticPaths() {
   return {
     // Render ZERO pages at build time
     paths: [],
-    fallback: true,
+    // { fallback: blocking } will server-render pages
+    // on-demand if the path doesn't exist. So all of our pages
+    // will be built on-demand.
+    fallback: 'blocking',
   }
 }
 
+// https://nextjs.org/docs/basic-features/data-fetching#incremental-static-regeneration
+// This function gets called at build time on server-side.
+// It may be called again, on a serverless function, if
+// revalidation is enabled and a new request comes in
 export async function getStaticProps({ params, preview = false }) {
   let slug
 
+  // Special case: home page
   if (!params?.slug) {
     slug = 'home'
     console.log('--- SLUG ---', slug)
@@ -53,17 +71,17 @@ export async function getStaticProps({ params, preview = false }) {
   }
 
   let data
-
+  // Check for page with matching slug
   try {
     data = await getPage(slug, preview)
     console.log('--- DATA ---', data)
   } catch (error) {
-    console.log(error)
+    console.log('--- ERROR ---', error)
+    return
   }
 
   if (!data) {
-    // notFound is not needed for fallback: false mode as only paths
-    // returned from getStaticPaths will be pre-rendered.
+    // `notFound` is an optional boolean value to allow the page to return a 404 status and page.
     return {
       notFound: true,
     }
@@ -74,6 +92,9 @@ export async function getStaticProps({ params, preview = false }) {
       preview,
       page: data.page,
     },
-    revalidate: 1,
+    // Next.js will attempt to re-generate the page:
+    // - When a request comes in
+    // - At most once every 10 seconds
+    revalidate: 10,
   }
 }
